6273e66b31d94633c9c630cf71943f2e012a7df7
ypinneka <yashwanth.260990@hotmail.com>
2023-08-07 12:39:14 -0700

    Add CDN for images

--- src/components/ComplexButton.jsx
+++ src/components/ComplexButton.jsx
@@ -6,7 +6,7 @@ import Typography from "@mui/material/Typography";
 
 const images = [
   {
-    url: "src/assets/breakfast.jpg",
+    url: "https://images.unsplash.com/photo-1504754524776-8f4f37790ca0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80",
     title: "Ready to Eat !",
     width: "40%",
   },
